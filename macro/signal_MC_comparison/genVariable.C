2967b9e408580fd8fea6621da524e29d9185db32
juwugit <jun-yi.wu@cern.ch>
2014-09-30 23:48:15 +0200

    signal_MC_comparison

---
+++ macro/signal_MC_comparison/genVariable.C
@@ -0,0 +1,242 @@
+//root -q -b -l genVariable.C++\(\"inputFile\"\,\"outputFile\"\)
+
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <TH1D.h>
+#include <TRandom.h>
+#include <TLorentzVector.h>
+#include <TFile.h>
+#include "/home/mattwu/CMS_Corp/XtoZh/macro/untuplizer.h"
+
+
+using namespace std;
+void genVariable(std::string inputFile, std::string outputFile){
+
+
+  //get TTree from file ...
+  TreeReader data(inputFile.data());
+
+
+  //histogram anoucement
+  TH1F* h_genXPt        = new TH1F("h_genXPt","",30,0,600);
+  h_genXPt->Sumw2();
+  h_genXPt->GetXaxis()->SetTitle("X P_{T} [GeV]");
+
+  TH1F* h_genXY         = new TH1F("h_genXY","",30,-3,3);
+  h_genXY->Sumw2();
+  h_genXY->GetXaxis()->SetTitle("X Rapidity");
+
+  TH1F* h_genXMass=new TH1F("h_genXMass","",1200,0,2400);
+  h_genXMass->Sumw2();
+  h_genXMass->GetXaxis()->SetTitle("X Mass [GeV]");
+
+  TH1F* h_genHMass=new TH1F("h_genHMass","",75,0,300);
+  h_genHMass->Sumw2();
+  h_genHMass->GetXaxis()->SetTitle("Higgs Mass [GeV]");
+
+  TH1F* h_genHPt        = new TH1F("h_genHPt","",60,0,1200);
+  h_genHPt->Sumw2();
+  h_genHPt->GetXaxis()->SetTitle("Higgs P_{T} [GeV]");
+
+  TH1F* h_genHEta       = new TH1F("h_genHEta","",30,-3,3);
+  h_genHEta->Sumw2();
+  h_genHEta->GetXaxis()->SetTitle("Higgs #eta");
+
+  TH1F* h_genDeltaRqq  = new TH1F("h_genDeltaRqq","",15,0,1.5);
+  h_genDeltaRqq->Sumw2();
+  h_genDeltaRqq->GetXaxis()->SetTitle("#DeltaR_{qq}");
+
+  TH1F* h_genZMass=new TH1F("h_genZMass","",50,0,200);
+  h_genZMass->Sumw2();
+  h_genZMass->GetXaxis()->SetTitle("Z Mass [GeV]");
+
+  TH1F* h_genZPt        = new TH1F("h_genZPt","",60,0,1200);
+  h_genZPt->Sumw2();
+  h_genZPt->GetXaxis()->SetTitle("Z P_{T} [GeV]");
+
+  TH1F* h_genZEta       = new TH1F("h_genZEta","",30,-3,3);
+  h_genZEta->Sumw2();
+  h_genZEta->GetXaxis()->SetTitle("Z #eta");
+
+  TH1F* h_genDeltaRll  = new TH1F("h_genDeltaRll","",15,0,1.5);
+  h_genDeltaRll->Sumw2();
+  h_genDeltaRll->GetXaxis()->SetTitle("#DeltaR_{ll}");
+
+
+
+
+
+
+
+
+  //Event loop
+  for(long jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
+
+    data.GetEntry(jEntry);
+
+    Int_t    nGenPar     = data.GetInt("nGenPar");
+    Float_t* genParE     = data.GetPtrFloat("genParE");
+    Float_t* genParPt    = data.GetPtrFloat("genParPt");
+    Float_t* genParEta   = data.GetPtrFloat("genParEta");
+    Float_t* genParPhi   = data.GetPtrFloat("genParPhi");
+    Float_t* genParM     = data.GetPtrFloat("genParM");
+    Int_t*   genParQ     = data.GetPtrInt("genParQ");
+    Int_t*   genParId    = data.GetPtrInt("genParId");
+    Int_t*   genParSt    = data.GetPtrInt("genParSt");
+    Int_t*   genMomParId = data.GetPtrInt("genMomParId");
+    
+    Int_t    nGenJet     = data.GetInt("nGenJet");
+    Float_t* genJetE     = data.GetPtrFloat("genJetE");
+    Float_t* genJetPt    = data.GetPtrFloat("genJetPt");
+    Float_t* genJetEta   = data.GetPtrFloat("genJetEta");
+    Float_t* genJetPhi   = data.GetPtrFloat("genJetPhi");
+
+
+
+
+    TLorentzVector X(0,0,0,0);
+    TLorentzVector l1(0,0,0,0);
+    TLorentzVector l2(0,0,0,0);
+    TLorentzVector q1(0,0,0,0);
+    TLorentzVector q2(0,0,0,0);
+
+
+    for(int i=0; i<nGenPar; i++){
+       for(int j=0; j<i; j++){
+
+        if(genParId[i]==9000001 && genParSt[i]==62){ 
+
+
+	  cout<<"XPt"<<genParPt[i]<<endl;
+          h_genXPt->Fill(genParPt[i]);
+	  h_genXMass->Fill(genParM[i]);
+
+          X.SetPtEtaPhiM(genParPt[i],genParEta[i],genParPhi[i],genParM[i]);
+          double XY=0;
+          XY=X.Rapidity();
+          h_genXY->Fill(XY);
+        }
+
+        if(genParId[i]==25 && genMomParId[i]==9000001){
+          h_genHPt->Fill(genParPt[i]);
+          h_genHEta->Fill(genParEta[i]);
+	  h_genHMass->Fill(genParM[i]);
+        }
+
+        if(genParId[i]==23 && genMomParId[i]==9000001){
+          h_genZPt->Fill(genParPt[i]);
+          h_genZEta->Fill(genParEta[i]);
+	  h_genZMass->Fill(genParM[i]);
+        }
+
+
+
+        // Z->ll      
+        if( (genParId[i]==11 || genParId[i]==13) && genMomParId[i]==23) 
+          l1.SetPtEtaPhiM(genParPt[i],genParEta[i],genParPhi[i],genParM[i]);
+       
+
+        if( (genParId[j]==-11 || genParId[j]==-13) && genMomParId[j]==23){
+          l2.SetPtEtaPhiM(genParPt[j],genParEta[j],genParPhi[j],genParM[j]);
+          double dRll=0;
+          dRll=l2.DeltaR(l1);
+          h_genDeltaRll->Fill(dRll);
+        }
+
+
+       // H-> qq 
+        if( (genParId[i]>=1 && genParId[i]<=5) && genMomParId[i]==25 ) 
+          q1.SetPtEtaPhiM(genParPt[i],genParEta[i],genParPhi[i],genParM[i]);
+   
+
+        else if( (genParId[i]<=-1 && genParId[i]>=-5) && genMomParId[i]==25 ){
+          q2.SetPtEtaPhiM(genParPt[i],genParEta[i],genParPhi[i],genParM[i]);
+          double dRqq=0;
+          dRqq=q2.DeltaR(q1);
+          h_genDeltaRqq->Fill(dRqq);
+        }
+
+
+      } // for2
+    } // for1
+
+
+
+
+
+
+  } //entries 
+
+    
+
+  
+  //normalization
+  float temp1=0;
+  temp1=h_genXPt->Integral();
+  h_genXPt->Scale(1/temp1);
+
+  float temp2=0;
+  temp2=h_genXY->Integral();
+  h_genXY->Scale(1/temp2);
+
+  float temp3=0;
+  temp3=h_genHPt->Integral();
+  h_genHPt->Scale(1/temp3);
+
+  float temp4=0;
+  temp4=h_genHEta->Integral();
+  h_genHEta->Scale(1/temp4);
+
+  float temp5=0;
+  temp5=h_genDeltaRqq->Integral();
+  h_genDeltaRqq->Scale(1/temp5);
+
+  float temp6=0;
+  temp6=h_genZPt->Integral();
+  h_genZPt->Scale(1/temp6);
+  
+  float temp7=0;
+  temp7=h_genZEta->Integral();
+  h_genZEta->Scale(1/temp7);
+
+  float temp8=0;
+  temp8=h_genDeltaRll->Integral();
+  h_genDeltaRll->Scale(1/temp8);
+
+  float temp9=0;
+  temp9=h_genXMass->Integral();
+  h_genXMass->Scale(1/temp9);
+  
+  float temp10=0;
+  temp10=h_genHMass->Integral();
+  h_genHMass->Scale(1/temp10);
+
+  float temp11=0;
+  temp11=h_genZMass->Integral();
+  h_genZMass->Scale(1/temp11);
+
+
+
+
+  //save output
+  TFile* outFile = new TFile(outputFile.data(),"recreate");
+
+  h_genXPt->Write();
+  h_genXY->Write();       
+  h_genXMass->Write();
+  h_genHMass->Write();
+  h_genHPt->Write();        
+  h_genHEta->Write();    
+  h_genDeltaRqq->Write(); 
+  h_genZMass->Write();
+  h_genZPt->Write();        
+  h_genZEta->Write();     
+  h_genDeltaRll->Write(); 
+
+
+  outFile->Close();
+
+
+}
---
+++ macro/signal_MC_comparison/stackGenVariables.C
@@ -0,0 +1,457 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <TPad.h>
+#include <TH1D.h>
+#include <TH1F.h>
+#include <TSystem.h>
+#include <TMath.h>
+#include <TFile.h>
+#include <TList.h>
+#include <TImage.h>
+#include <TLine.h>
+#include <TAxis.h>
+#include <TGraph.h>
+#include <TStyle.h>
+#include <TChain.h>
+#include <THStack.h>
+#include <TLegend.h>
+#include <TCanvas.h>
+#include <TBranch.h>
+#include <TRandom.h>
+#include <TVectorT.h>
+#include <TProfile.h>
+#include <TLorentzVector.h>
+#include <TSystemDirectory.h>
+#include <TGraphAsymmErrors.h>
+
+
+
+void myPlotL(TH1D*, TH1D*);
+void myPlotR(TH1D*, TH1D*);
+void myPlotN(TH1D*, TH1D*);
+void myRatio(TH1D*, TH1D*, Double_t , Double_t );
+
+
+void stackGenVariables(){
+
+
+  TFile *f1 = TFile::Open("rootfile/delpanj_my_xzh_LO_M1000.root");
+  TFile *f2 = TFile::Open("rootfile/delpanj_v2_ZPrime_hZ_qqll_LHC8_M1000.root");
+
+
+  gStyle->SetOptStat(0);
+  //gStyle->SetPadGridY(kTRUE);
+  //gStyle->SetPadGridX(kTRUE);
+
+
+
+  TCanvas* c1 = new TCanvas("c1", "", 0, 0, 1500, 750);
+
+  c1->Divide(2,2,0,0);
+
+  //-------------------------------------------//
+
+  c1->cd(1);
+  myPlotL( ((TH1D*)(f1->Get("h_genHPt"))), 
+	  ((TH1D*)(f2->Get("h_genHPt")))
+	  );
+  
+  gPad->SetRightMargin(0.02);
+  
+  //-------------------------------------------//
+  
+  c1->cd(2);
+  myPlotN( ((TH1D*)(f1->Get("h_genZPt"))),
+          ((TH1D*)(f2->Get("h_genZPt")))
+          );
+  
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+  
+  //-------------------------------------------//
+
+  c1->cd(3);
+  myRatio( ((TH1D*)(f1->Get("h_genHPt"))),
+	   ((TH1D*)(f2->Get("h_genHPt"))),
+	   0,1200);
+  
+  
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+
+
+
+  //-------------------------------------------//
+  
+  c1->cd(4);
+  myRatio( ((TH1D*)(f1->Get("h_genZPt"))),
+           ((TH1D*)(f2->Get("h_genZPt"))),
+	   0,1200);
+  
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+
+  
+  gSystem->ProcessEvents();
+  TImage *img1 = TImage::Create();
+  img1->FromPad(c1);
+  img1->WriteImage("h_genPt.png");
+  delete c1;
+  delete img1;
+
+
+  //-------------------------------------------//
+
+
+
+  
+
+  TCanvas* c2 = new TCanvas("c2", "", 0, 0, 1500, 750);
+
+  c2->Divide(2,2,0,0);
+
+
+
+  c2->cd(1);
+  myPlotL( ((TH1D*)(f1->Get("h_genHEta"))),
+          ((TH1D*)(f2->Get("h_genHEta")))
+          );
+
+  gPad->SetRightMargin(0.02);
+
+
+  //-------------------------------------------//
+
+  c2->cd(2);
+  myPlotN( ((TH1D*)(f1->Get("h_genZEta"))),
+          ((TH1D*)(f2->Get("h_genZEta")))
+          );
+
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+  //-------------------------------------------//
+  
+  c2->cd(3);
+  myRatio( ((TH1D*)(f1->Get("h_genHEta"))),
+           ((TH1D*)(f2->Get("h_genHEta"))),
+           -3,3);
+
+
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+
+  //--------------------------------------------//
+
+  c2->cd(4);
+  myRatio( ((TH1D*)(f1->Get("h_genZEta"))),
+           ((TH1D*)(f2->Get("h_genZEta"))),
+           -3,3);
+
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+
+
+  gSystem->ProcessEvents();
+  TImage *img2 = TImage::Create();
+  img2->FromPad(c2);
+  img2->WriteImage("h_genEta.png");
+  delete c2;
+  delete img2;
+  
+
+
+
+
+  TCanvas* c3 = new TCanvas("c3", "", 0, 0, 1500, 750);
+
+  c3->Divide(2,2,0,0);
+
+
+
+  c3->cd(1);
+  myPlotR( ((TH1D*)(f1->Get("h_genDeltaRqq"))),
+          ((TH1D*)(f2->Get("h_genDeltaRqq")))
+          );
+
+  gPad->SetRightMargin(0.02);
+
+  //-------------------------------------------//                                                                    
+
+  c3->cd(2);
+  myPlotN( ((TH1D*)(f1->Get("h_genDeltaRll"))),
+	   ((TH1D*)(f2->Get("h_genDeltaRll")))
+	   );
+
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+  //-------------------------------------------//                                                                    
+
+  c3->cd(3);
+  myRatio( ((TH1D*)(f1->Get("h_genDeltaRqq"))),
+           ((TH1D*)(f2->Get("h_genDeltaRqq"))),
+           0,1.5);
+
+
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+
+  //--------------------------------------------//               
+
+  c3->cd(4);
+  myRatio( ((TH1D*)(f1->Get("h_genDeltaRll"))),
+           ((TH1D*)(f2->Get("h_genDeltaRll"))),
+           0,1.5);
+
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+
+
+  gSystem->ProcessEvents();
+  TImage *img3 = TImage::Create();
+  img3->FromPad(c3);
+  img3->WriteImage("h_genDeltaR.png");
+  delete c3;
+  delete img3;
+
+
+
+  TCanvas* c4 = new TCanvas("c4", "", 0, 0, 1500, 750);
+
+  c4->Divide(2,2,0,0);
+
+  //-------------------------------------------//
+
+  c4->cd(1);
+  myPlotR( ((TH1D*)(f1->Get("h_genXPt"))), 
+	  ((TH1D*)(f2->Get("h_genXPt")))
+	  );
+  
+  gPad->SetRightMargin(0.02);
+  
+  //-------------------------------------------//
+  
+  c4->cd(2);
+  myPlotN( ((TH1D*)(f1->Get("h_genXY"))),
+          ((TH1D*)(f2->Get("h_genXY")))
+          );
+  
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+  
+  //-------------------------------------------//
+
+  c4->cd(3);
+  myRatio( ((TH1D*)(f1->Get("h_genXPt"))),
+	   ((TH1D*)(f2->Get("h_genXPt"))),
+	   0,600);
+  
+  
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+
+
+
+  //-------------------------------------------//
+  
+  c4->cd(4);
+  myRatio( ((TH1D*)(f1->Get("h_genXY"))),
+           ((TH1D*)(f2->Get("h_genXY"))),
+	   3,-3);
+  
+  gPad->SetTickx();
+  gPad->SetRightMargin(0.02);
+  gPad->SetLeftMargin(0.07);
+
+
+  
+  gSystem->ProcessEvents();
+  TImage *img4 = TImage::Create();
+  img4->FromPad(c4);
+  img4->WriteImage("h_genX.png");
+  delete c4;
+  delete img4;
+
+
+
+
+
+}
+
+
+
+void myPlotL(TH1D* h_sig, TH1D* h_sigold){
+
+
+  h_sigold->SetFillColor(kCyan-4);
+  h_sigold->SetLineColor(kBlack);
+
+
+  h_sig->SetLineColor(1);
+  h_sig->SetMarkerStyle(8);
+  h_sig->SetMarkerSize(1);
+
+
+  Double_t ymin = -0.03*(h_sigold->GetMaximum());
+
+  h_sigold->SetMinimum(ymin);
+  //h_sig->GetXaxis()->SetRangeUser(min,max);
+  //h_sigold->SetLabelSize(0);
+  //h_sigold->SetXTitle("");
+
+
+  //h_sig->Draw("e1"); 
+  h_sigold->Draw("histe");
+  h_sig->Draw("e1same");
+
+
+
+  TLegend *leg = new TLegend(0.15, 0.8, 0.4, 1);
+
+  leg->SetFillStyle(1001);
+  leg->SetFillColor(10);
+  leg->SetBorderSize(1);
+  leg->AddEntry(h_sigold,"signal MC", "f"); 
+  leg->AddEntry(h_sig, "new signal MC", "lp");
+  leg->Draw();
+
+}
+
+
+
+
+void myRatio(TH1D* h_sig, TH1D* h_sigold, Double_t min , Double_t max){
+  
+  
+
+  TH1D* h_ratio = (TH1D*)h_sigold->Clone("h_ratio");
+  h_ratio->Reset();
+  //h_ratio->Sumw2();
+
+  //h_ratio->Divide(h_sig,h_sigold,1,1);
+  
+  Int_t nbin=h_ratio->GetNbinsX();
+  Double_t ratio[nbin];
+  Double_t error[nbin];
+  Double_t numer_nbincontent[nbin];
+  Double_t denom_nbincontent[nbin];
+  Double_t numer_binerror[nbin];
+  Double_t denom_binerror[nbin];
+
+  for(Int_t i=1; i<=nbin; i++){
+
+
+    numer_nbincontent[i] = h_sig->GetBinContent(i);
+    denom_nbincontent[i] = h_sigold->GetBinContent(i);
+    numer_binerror[i] = h_sig->GetBinError(i);
+    denom_binerror[i] = h_sigold->GetBinError(i);
+
+
+    if(denom_nbincontent[i]<=0 || numer_nbincontent[i]<=0) continue;
+    if(denom_binerror[i]<=0 || numer_binerror[i]<=0) continue;
+
+
+    ratio[i] = numer_nbincontent[i]/denom_nbincontent[i];
+    error[i] = (ratio[i])*sqrt(pow(numer_binerror[i]/numer_nbincontent[i],2)+pow(denom_binerror[i]/denom_nbincontent[i],2));
+
+
+    h_ratio->SetBinContent(i,ratio[i]);
+    h_ratio->SetBinError(i,error[i]);
+   
+
+
+  }
+
+
+
+
+ 
+  //h_ratio->SetLineColor(1);
+  h_ratio->SetMarkerStyle(8);
+  h_ratio->SetMarkerSize(1);
+
+  h_ratio->GetXaxis()->SetRangeUser(min,max);
+  h_ratio->GetYaxis()->SetRangeUser(0.2,1.7);
+  h_ratio->SetYTitle("new MC/MC");
+  h_ratio->Draw();
+
+
+  TLine* l2 = new TLine(min,1.,max,1.);
+  l2->SetLineColor(4);
+  l2->SetLineStyle(3);
+  l2->Draw("same");
+
+
+}
+
+
+
+void myPlotN(TH1D* h_sig, TH1D* h_sigold){
+
+
+  h_sigold->SetFillColor(kCyan-4);
+  h_sigold->SetLineColor(kBlack);
+
+
+  h_sig->SetLineColor(1);
+  h_sig->SetMarkerStyle(8);
+  h_sig->SetMarkerSize(1);
+
+
+  Double_t ymin = -0.03*(h_sigold->GetMaximum());
+
+  h_sigold->SetMinimum(ymin);
+
+  //h_sig->Draw("e1");                                                                                              
+  h_sigold->Draw("histe");
+  h_sig->Draw("e1same");
+
+}
+
+
+void myPlotR(TH1D* h_sig, TH1D* h_sigold){
+
+
+  h_sigold->SetFillColor(kCyan-4);
+  h_sigold->SetLineColor(kBlack);
+
+
+  h_sig->SetLineColor(1);
+  h_sig->SetMarkerStyle(8);
+  h_sig->SetMarkerSize(1);
+
+
+  Double_t ymin = -0.03*(h_sigold->GetMaximum());
+
+  h_sigold->SetMinimum(ymin);
+
+  //h_sig->Draw("e1");                                                                                             
+  h_sigold->Draw("histe");
+  h_sig->Draw("e1same");
+
+
+  
+  TLegend *leg = new TLegend(0.73, 0.8, 0.98, 1);
+
+  leg->SetFillStyle(1001);
+  leg->SetFillColor(10);
+  leg->SetBorderSize(1);
+  leg->AddEntry(h_sigold,"signal MC", "f");
+  leg->AddEntry(h_sig, "new signal MC", "lp");
+  leg->Draw();
+
+
+
+
+}
