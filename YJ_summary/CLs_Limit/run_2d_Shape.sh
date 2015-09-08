5bcc70426dde67513cb867782a1ea8cf8161825a
juwugit <jun-yi.wu@cern.ch>
2015-09-08 14:07:02 +0800

    remove WW background

--- YJ_summary/CLs_Limit/run_2d_Shape.sh
+++ YJ_summary/CLs_Limit/run_2d_Shape.sh
@@ -4,7 +4,7 @@ rm -rf *Testing_0819*.txt
 rm -rf *_${xmass}*.root
 rm -rf *output_0819*.root
 rm -rf Shape1d_0819.root
-root -b -l -q ProjectHistograms_YJ.C+\(\"../result_cuts/ZPrime_ZH_lljj_M${xmass}_Cen.root\"\)
+root -b -l -q ProjectHistograms_YJ.C+\(\"../result_cuts/ZPrime_ZH_lljj_M${xmass}_El_Cen.root\"\)
 root -b -l -q Change_data_obs_name.C+
 python MakeDataCardsYJ.py
 combineCards.py Name1=Datacard_Testing_0819_1.txt Name2=Datacard_Testing_0819_2.txt Name3=Datacard_Testing_0819_3.txt  Name4=Datacard_Testing_0819_4.txt   Name5=Datacard_Testing_0819_5.txt > Mycard_YJ.txt
