b95376d9cefe28e9fd9d85e4738da386f40d7a41
juwugit <jun-yi.wu@cern.ch>
2015-09-11 18:00:22 +0200

    put back

--- YJ_summary/CLs_Limit/run_2d_Shape.sh
+++ YJ_summary/CLs_Limit/run_2d_Shape.sh
@@ -8,10 +8,10 @@ root -b -l -q ProjectHistograms_YJ.C+\(\"../result_cuts/ZPrime_ZH_lljj_M${xmass}
 root -b -l -q Change_data_obs_name.C+
 python MakeDataCardsYJ.py
 combineCards.py Name1=Datacard_Testing_0819_1.txt Name2=Datacard_Testing_0819_2.txt Name3=Datacard_Testing_0819_3.txt  Name4=Datacard_Testing_0819_4.txt   Name5=Datacard_Testing_0819_5.txt > Mycard_YJ.txt
-combine -M Asymptotic Mycard_YJ.txt -n shape_2d_${xmass}
+#combine -M Asymptotic Mycard_YJ.txt -n shape_2d_${xmass}
 combine -M Asymptotic Datacard_1dshape_Testing_0819.txt -n shape_1d_${xmass}
-combine -M Asymptotic Datacard_counting_Testing_0819.txt -n counting_${xmass}
+#combine -M Asymptotic Datacard_counting_Testing_0819.txt -n counting_${xmass}
 combine -M ProfileLikelihood Mycard_YJ.txt -n shape_2d_${xmass} --signif --pvalue -t -1 --expectSignal=1 
-combine -M ProfileLikelihood Datacard_1dshape_Testing_0819.txt -n shape_1d_${xmass} --signif --pvalue -t -1 --expectSignal=1 
-combine -M ProfileLikelihood Datacard_counting_Testing_0819.txt -n counting_${xmass} --signif --pvalue -t -1 --expectSignal=1 
+#combine -M ProfileLikelihood Datacard_1dshape_Testing_0819.txt -n shape_1d_${xmass} --signif --pvalue -t -1 --expectSignal=1 
+#combine -M ProfileLikelihood Datacard_counting_Testing_0819.txt -n counting_${xmass} --signif --pvalue -t -1 --expectSignal=1 
 
