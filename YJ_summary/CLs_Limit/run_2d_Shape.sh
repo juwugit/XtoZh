a3faf3ab8af3c81594899d3c0b78579042b23ae2
juwugit <jun-yi.wu@cern.ch>
2015-09-14 08:55:17 +0200

    push

--- YJ_summary/CLs_Limit/run_2d_Shape.sh
+++ YJ_summary/CLs_Limit/run_2d_Shape.sh
@@ -1,17 +1,17 @@
+#xmass=$1
 
-xmass=900 
 rm -rf *Testing_0819*.txt
-rm -rf *_${xmass}*.root
+rm -rf *_$1*.root
 rm -rf *output_0819*.root
 rm -rf Shape1d_0819.root
-root -b -l -q ProjectHistograms_YJ.C+\(\"../result_cuts/ZPrime_ZH_lljj_M${xmass}_El_Cen.root\"\)
+root -b -l -q ProjectHistograms_YJ.C+\(\"../result_cuts/ZPrime_ZH_lljj_M$1_El_Cen.root\"\)
 root -b -l -q Change_data_obs_name.C+
 python MakeDataCardsYJ.py
 combineCards.py Name1=Datacard_Testing_0819_1.txt Name2=Datacard_Testing_0819_2.txt Name3=Datacard_Testing_0819_3.txt  Name4=Datacard_Testing_0819_4.txt   Name5=Datacard_Testing_0819_5.txt > Mycard_YJ.txt
-#combine -M Asymptotic Mycard_YJ.txt -n shape_2d_${xmass}
-combine -M Asymptotic Datacard_1dshape_Testing_0819.txt -n shape_1d_${xmass}
-#combine -M Asymptotic Datacard_counting_Testing_0819.txt -n counting_${xmass}
-combine -M ProfileLikelihood Mycard_YJ.txt -n shape_2d_${xmass} --signif --pvalue -t -1 --expectSignal=1 
-#combine -M ProfileLikelihood Datacard_1dshape_Testing_0819.txt -n shape_1d_${xmass} --signif --pvalue -t -1 --expectSignal=1 
-#combine -M ProfileLikelihood Datacard_counting_Testing_0819.txt -n counting_${xmass} --signif --pvalue -t -1 --expectSignal=1 
+combine -M Asymptotic Mycard_YJ.txt -n shape_2d_$1
+combine -M Asymptotic Datacard_1dshape_Testing_0819.txt -n shape_1d_$1
+combine -M Asymptotic Datacard_counting_Testing_0819.txt -n counting_$1
+combine -M ProfileLikelihood Mycard_YJ.txt -n shape_2d_$1 --signif --pvalue -t -1 --expectSignal=1 
+combine -M ProfileLikelihood Datacard_1dshape_Testing_0819.txt -n shape_1d_$1 --signif --pvalue -t -1 --expectSignal=1 
+combine -M ProfileLikelihood Datacard_counting_Testing_0819.txt -n counting_$1 --signif --pvalue -t -1 --expectSignal=1 
 
--- YJ_summary/CLs_Limit/run_2d_Shape1half.sh
+++
@@ -1,9 +0,0 @@
-
-xmass=900 
-rm -rf *Testing_0819*.txt
-rm -rf *_${xmass}*.root
-rm -rf *output_0819*.root
-rm -rf Shape1d_0819.root
-root -b -l -q ProjectHistograms_YJ.C+\(\"../result_cuts/ZPrime_ZH_lljj_M${xmass}_El_Cen.root\"\)
-root -b -l -q Change_data_obs_name.C+
-python MakeDataCardsYJ.py
