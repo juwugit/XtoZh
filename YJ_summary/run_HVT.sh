91abee9525b3af61c81727af39398dd329d73017
juwugit <jun-yi.wu@cern.ch>
2015-09-15 10:04:35 +0200

    new sh format

--- YJ_summary/run_HVT.sh
+++ YJ_summary/run_HVT.sh
@@ -3,17 +3,16 @@
 # $3 = 0/1/-1 ; JetEnScale
 # example: $sh run_HVT.sh El Cen 0
 
+
+# Data
 rm -rf result_cuts/*$1_$2.root
 root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_data_Double$1_A.root\"\,\"result_cuts/dataA_$1_$2.root\"\)
 root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_data_Double$1_B.root\"\,\"result_cuts/dataB_$1_$2.root\"\)
 root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_data_Double$1_C.root\"\,\"result_cuts/dataC_$1_$2.root\"\)
 root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_data_Double$1_D.root\"\,\"result_cuts/dataD_$1_$2.root\"\)
 
-# hadd all data
-hadd result_cuts/data_$1_$2.root result_cuts/data*_$1_$2.root
-
 # Bkg MC
-root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuple/delpanj_v4_TTTo2L2Nu2B.root\"\,\"result_cuts/TH2_TTTo2L2Nu2B_$1_$2.root\"\)
+root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_TTTo2L2Nu2B.root\"\,\"result_cuts/TH2_TTTo2L2Nu2B_$1_$2.root\"\)
 root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_WW_pythia_filtered.root\"\,\"result_cuts/TH2_DiBWW_$1_$2.root\"\)
 root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_WZ_pythia_filtered.root\"\,\"result_cuts/TH2_DiBWZ_$1_$2.root\"\)
 root -b -l -q Main_analyzer/recoXMassCSV.C++\($3\,\"/data1/delpanjNtuples/delpanj_v4_ZZ_pythia_filtered.root\"\,\"result_cuts/TH2_DiBZZ_$1_$2.root\"\)
