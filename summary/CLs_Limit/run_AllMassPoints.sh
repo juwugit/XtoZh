60275b9d7ea7ab38a17cbb55bbee7e6cb94635cf
juwugit <jun-yi.wu@cern.ch>
2015-09-19 14:13:54 +0200

    add all masspoint

--- summary/CLs_Limit/run_AllMassPoints.sh
+++ summary/CLs_Limit/run_AllMassPoints.sh
@@ -3,7 +3,14 @@
 
 sh run_2d_Shape.sh $1 800
 sh run_2d_Shape.sh $1 900
+sh run_2d_Shape.sh $1 1000
 sh run_2d_Shape.sh $1 1100
+sh run_2d_Shape.sh $1 1200
+sh run_2d_Shape.sh $1 1300
 sh run_2d_Shape.sh $1 1400
+sh run_2d_Shape.sh $1 1500
+sh run_2d_Shape.sh $1 1600
 sh run_2d_Shape.sh $1 1700
+sh run_2d_Shape.sh $1 1800
 sh run_2d_Shape.sh $1 1900
+sh run_2d_Shape.sh $1 2000
