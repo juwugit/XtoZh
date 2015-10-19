56f0517d077e9fec97f19e60849955b58fddf890
juwugit <jun-yi.wu@cern.ch>
2015-10-19 09:06:23 +0200

    add PDF info

---
+++ summary/Main_analyzer/runPDF.C
@@ -0,0 +1,21 @@
+#include <TStopwatch.h>
+#include <TROOT.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void runPDF(std::string filename)
+{
+  TStopwatch* myWatch = new TStopwatch();
+  myWatch->Start();
+  gROOT->ProcessLine(".L ${LHAPDFDIR}/lib/libLHAPDF.so");
+
+  gROOT->ProcessLine(".L Main_analyzer/myLHAPDF_reweighing.C++");
+
+  gROOT->ProcessLine(Form("myLHAPDF_reweighing(\"%s\")",filename.data()));
+
+  myWatch->Stop();
+  cout << myWatch->RealTime() << " seconds has passed! " << endl; 
+
+}
