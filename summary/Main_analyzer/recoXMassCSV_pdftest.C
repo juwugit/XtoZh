989a2c2968f3c5857ce5632d3935d4c7bacce9ca
juwugit <jun-yi.wu@cern.ch>
2015-10-29 09:59:50 +0100

    CT10+CSV

--- summary/Main_analyzer/recoXMassCSV_pdftest.C
+++ summary/Main_analyzer/recoXMassCSV_pdftest.C
@@ -29,7 +29,7 @@ using namespace std;
 
 
 // maximum number of PDFs allowed is 5
-const int NPDFS=5;
+const int NPDFS=3;
 const int defaultIndex=1;
 
 
@@ -255,7 +255,7 @@ void recoXMassCSV_pdftest(Int_t scaleMode, std::string inputFile, std::string ou
   MyPDF* nnpdf21lo;
   MyPDF* mstw2008nlo;
   MyPDF* nnpdf23nlo;
-  //MyPDF2* ct10nlo;
+  MyPDF2* ct10nlo;
 
   if(isV5){
     for(int i=0;i<NPDFS ;i++){
@@ -263,11 +263,11 @@ void recoXMassCSV_pdftest(Int_t scaleMode, std::string inputFile, std::string ou
       h_sigSubjetCSVPDF[i]=(TH1F*)h_sigSubjetCSV->Clone(Form("h_sigSubjetCSVPDF%d",i));
     }
 
-    mstw2008lo = new MyPDF("MSTW2008lo68cl.LHgrid",2);
-    nnpdf21lo = new MyPDF("NNPDF21_lo_as_0119_100.LHgrid",3);
+    //mstw2008lo = new MyPDF("MSTW2008lo68cl.LHgrid",2);
+    //nnpdf21lo = new MyPDF("NNPDF21_lo_as_0119_100.LHgrid",3);
     mstw2008nlo = new MyPDF("MSTW2008nlo68cl.LHgrid",4);
-    nnpdf23nlo = new MyPDF("NNPDF23_nlo_collider_as_0118.LHgrid",5);
-    //ct10nlo = new MyPDF2("CT10.LHgrid",2);
+    //nnpdf23nlo = new MyPDF("NNPDF23_nlo_collider_as_0118.LHgrid",5);
+    ct10nlo = new MyPDF2("CT10.LHgrid",2);
   }
   
 
@@ -413,11 +413,11 @@ void recoXMassCSV_pdftest(Int_t scaleMode, std::string inputFile, std::string ou
 
 	double weight_pdf[NPDFS]={
 	  1.0,
-	  mstw2008lo->weight(pdfInfo,0),
-	  nnpdf21lo->weight(pdfInfo,0),
+	  //mstw2008lo->weight(pdfInfo,0),
+	  //nnpdf21lo->weight(pdfInfo,0),
 	  mstw2008nlo->weight(pdfInfo,0),
-	  nnpdf23nlo->weight(pdfInfo,0)
-	  //((mstw2008nlo->weight(pdfInfo,0))*(ct10nlo->weight2(pdfInfo,0)))
+	  //nnpdf23nlo->weight(pdfInfo,0)
+	  ((mstw2008nlo->weight(pdfInfo,0))*(ct10nlo->weight2(pdfInfo,0)))
 	};
 	
 	for(int i=0;i<NPDFS;i++)
@@ -453,16 +453,17 @@ void recoXMassCSV_pdftest(Int_t scaleMode, std::string inputFile, std::string ou
 	if(prunedmass>110 && prunedmass<140){
 
 	  h_sigMxSubjetCSV->Fill(XMass, SubjetCSV[leadjet][0],weight*PU_weight);
+	  h_sigSubjetCSV->Fill(SubjetCSV[leadjet][0],weight*PU_weight);
 
 	  if(isV5){
 	    
 	    double weight_pdf[NPDFS]={
 	      1.0,
-	      mstw2008lo->weight(pdfInfo,0),
-	      nnpdf21lo->weight(pdfInfo,0),
+	      //mstw2008lo->weight(pdfInfo,0),
+	      //nnpdf21lo->weight(pdfInfo,0),
 	      mstw2008nlo->weight(pdfInfo,0),
-	      nnpdf23nlo->weight(pdfInfo,0)
-	      //((mstw2008nlo->weight(pdfInfo,0))*(ct10nlo->weight2(pdfInfo,0)))
+	      //nnpdf23nlo->weight(pdfInfo,0)
+	      ((mstw2008nlo->weight(pdfInfo,0))*(ct10nlo->weight2(pdfInfo,0)))
 	    };
 	    
 	    for(int i=0;i<NPDFS;i++)
@@ -477,16 +478,17 @@ void recoXMassCSV_pdftest(Int_t scaleMode, std::string inputFile, std::string ou
 	if(prunedmass>110 && prunedmass<140){
 
 	  h_sigMxSubjetCSV->Fill(XMass, SubjetCSV[leadjet][1],weight*PU_weight);
-	  
+	  h_sigSubjetCSV->Fill(SubjetCSV[leadjet][1],weight*PU_weight);	  
+
 	  if(isV5){
 	    
 	    double weight_pdf[NPDFS]={
 	      1.0,
-	      mstw2008lo->weight(pdfInfo,0),
-	      nnpdf21lo->weight(pdfInfo,0),
+	      //mstw2008lo->weight(pdfInfo,0),
+	      //nnpdf21lo->weight(pdfInfo,0),
 	      mstw2008nlo->weight(pdfInfo,0),
-	      nnpdf23nlo->weight(pdfInfo,0)
-	      //((mstw2008nlo->weight(pdfInfo,0))*(ct10nlo->weight2(pdfInfo,0)))
+	      //nnpdf23nlo->weight(pdfInfo,0)
+	      ((mstw2008nlo->weight(pdfInfo,0))*(ct10nlo->weight2(pdfInfo,0)))
 	    };
 	    
 	    for(int i=0;i<NPDFS;i++)
