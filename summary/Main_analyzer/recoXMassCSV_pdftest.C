bafe8107469fc348a2bd912ce8742d56248ebb36
juwugit <jun-yi.wu@cern.ch>
2015-11-06 06:41:05 +0100

    CT10 CSV 0.6~0.8

--- summary/Main_analyzer/recoXMassCSV_pdftest.C
+++ summary/Main_analyzer/recoXMassCSV_pdftest.C
@@ -470,7 +470,7 @@ void recoXMassCSV_pdftest(Int_t scaleMode, std::string inputFile, std::string ou
 	    for(int i=0;i<NPDFS;i++)
 	      h_sigSubjetCSVPDF[i]->Fill(SubjetCSV[leadjet][0],weight*PU_weight*weight_pdf[i]);
 	    
-	    if(SubjetCSV[leadjet][0]>0.4 && SubjetCSV[leadjet][0]<0.6)
+	    if(SubjetCSV[leadjet][0]>0.6 && SubjetCSV[leadjet][0]<0.8)
 	      for(int i=0;i<NPDFS;i++)
 		h_sigXMassPDF[i]->Fill(XMass,weight*PU_weight*weight_pdf[i]);
 	    
@@ -500,7 +500,7 @@ void recoXMassCSV_pdftest(Int_t scaleMode, std::string inputFile, std::string ou
 	    for(int i=0;i<NPDFS;i++)
 	      h_sigSubjetCSVPDF[i]->Fill(SubjetCSV[leadjet][1],weight*PU_weight*weight_pdf[i]);
 
-	    if(SubjetCSV[leadjet][1]>0.4 && SubjetCSV[leadjet][1]<0.6)
+	    if(SubjetCSV[leadjet][1]>0.6 && SubjetCSV[leadjet][1]<0.8)
 	      for(int i=0;i<NPDFS;i++)
 		h_sigXMassPDF[i]->Fill(XMass,weight*PU_weight*weight_pdf[i]);
 	    
