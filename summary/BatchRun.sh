f49cc422ebd41047d24edaefe06be3568f86dd55
juwugit <jun-yi.wu@cern.ch>
2015-12-01 09:18:13 +0100

    batch

--- summary/BatchRun.sh
+++ summary/BatchRun.sh
@@ -4,4 +4,4 @@ cd $1
 
 setenv slc6_amd64_gcc472; eval `scramv1 runtime -csh`
 
-sh run_HVT_All.sh
+sh run_HVT.sh El Cen 0
--- summary/Main_analyzer/runPDF_CT10.C
+++ summary/Main_analyzer/runPDF_CT10.C
@@ -9,7 +9,7 @@ void runPDF_CT10(Int_t scaleMode,std::string inputFile,std::string outputFile){
 
   TStopwatch* myWatch = new TStopwatch();
   myWatch->Start();
-  gROOT->ProcessLine(".L /data4/juwu/CMSSW_5_3_20/src/test_pdf/LHAPDF/lib/libLHAPDF.so");
+  gROOT->ProcessLine(".L /afs/cern.ch/work/j/juwu/workspace/CMSSW_5_3_20/src/test_pdf/LHAPDF/lib/libLHAPDF.so");
 
   //gROOT->ProcessLine(".L Main_analyzer/myLHAPDF_reweighing.C++");
   //gROOT->ProcessLine(Form("myLHAPDF_reweighing(\"%s\")",filename.data()));
