d1e6db50adcf91cba48042a0a61645373c46b15c
juwugit <jun-yi.wu@cern.ch>
2015-12-09 09:38:11 +0100

    hadd mu el

---
+++ summary/Merge2LepCH.sh
@@ -0,0 +1,16 @@
+hadd Bkg_estimation/Total_data_FN_$1.root Bkg_estimation/Total_data_El_$1.root Bkg_estimation/Total_data_Mu_$1.root
+
+
+hadd result_cuts/ZPrime_ZH_lljj_M800_FN_$1.root result_cuts/ZPrime_ZH_lljj_M800_El_$1.root result_cuts/ZPrime_ZH_lljj_M800_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M900_FN_$1.root result_cuts/ZPrime_ZH_lljj_M900_El_$1.root result_cuts/ZPrime_ZH_lljj_M900_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1000_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1000_El_$1.root result_cuts/ZPrime_ZH_lljj_M1000_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1100_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1100_El_$1.root result_cuts/ZPrime_ZH_lljj_M1100_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1200_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1200_El_$1.root result_cuts/ZPrime_ZH_lljj_M1200_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1300_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1300_El_$1.root result_cuts/ZPrime_ZH_lljj_M1300_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1400_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1400_El_$1.root result_cuts/ZPrime_ZH_lljj_M1400_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1500_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1500_El_$1.root result_cuts/ZPrime_ZH_lljj_M1500_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1600_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1600_El_$1.root result_cuts/ZPrime_ZH_lljj_M1600_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1700_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1700_El_$1.root result_cuts/ZPrime_ZH_lljj_M1700_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1800_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1800_El_$1.root result_cuts/ZPrime_ZH_lljj_M1800_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M1900_FN_$1.root result_cuts/ZPrime_ZH_lljj_M1900_El_$1.root result_cuts/ZPrime_ZH_lljj_M1900_Mu_$1.root
+hadd result_cuts/ZPrime_ZH_lljj_M2000_FN_$1.root result_cuts/ZPrime_ZH_lljj_M2000_El_$1.root result_cuts/ZPrime_ZH_lljj_M2000_Mu_$1.root
